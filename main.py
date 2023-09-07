f9e48cf20011335c391669eaa3f8dd453c981390
Dedi Kurniawan <dedi.kurniawan@aditya.co.id>
2023-09-07 14:00:07 +0700

    feat:initiate python

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+# print('test')
+# print(os.path.abspath(os.getcwd()))
+# print(os.path.dirname(__file__))
+
+# a = 1
+
+# if a:
+#     print('satu')
+# else:
+#     print('bukan satu')
+
+
+
+b= '1'
+b = int(b)
+
+print(b + 2)
